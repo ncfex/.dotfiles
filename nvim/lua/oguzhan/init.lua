56da7141fac81943e0c6a70e272ea86e4e801a39
oguzhan <oguzhan@ait.com.tr>
2023-11-22 17:49:04 +0300

    Add nvim-tree setup and update netrw settings

--- nvim/lua/oguzhan/init.lua
+++ nvim/lua/oguzhan/init.lua
@@ -1,6 +1,54 @@
 require("oguzhan.remap")
 require("oguzhan.set")
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup({
+  filters = {
+    custom = { ".git" },
+    exclude = { ".gitignore" },
+  },
+  update_cwd = true,
+  renderer = {
+    add_trailing = false,
+    group_empty = false,
+    highlight_git = false,
+    highlight_opened_files = "none",
+    root_folder_modifier = ":t",
+    indent_markers = {
+      enable = false,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        none = "  ",
+      },
+    },
+  },
+  diagnostics = {
+    enable = false,
+  },
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = {},
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 500,
+  },
+  view = {
+    hide_root_folder = false,
+    side = "left",
+    number = false,
+    relativenumber = false,
+  },
+})
+
+
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
